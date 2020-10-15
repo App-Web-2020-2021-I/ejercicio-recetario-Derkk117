6f40e4359a2a84ab8a2af1869b530b4c66a6d55d
derkk117 <angel.aguilar@111btec.com>
2020-10-15 13:44:36 -0500

    obtenemos el texto correcto del select

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
 $(document).ready(function() {
+    let buttonCollapse = $("#collapse");
+    let containerRecetario = $("#recetario");
+    let containerAgregar = $("#Agregar");
+    let separador = $("#separadorVertical");
 
+    buttonCollapse.click(function() {
+        if (containerRecetario[0].style.width === "100%") {
+            containerRecetario[0].style.width = "74.5%";
+            containerAgregar.show(200);
+            separador.show();
+        } else {
+            containerAgregar.hide(400);
+            separador.hide();
+            containerRecetario[0].style.width = "100%";
+        }
+    });
+
+
+
+
+    $("#form-receta").submit(function(e) {
+        e.preventDefault();
+
+        let nombre = $("#nombre").val();
+        let categoria = $("#categoria option:selected")[0].text;
+        let imagen = $("#imagen").val();
+
+        let receta = {
+            nombre: nombre,
+            categoria: categoria,
+            imagen: imagen,
+        };
+
+        console.log(receta);
+    });
 });
